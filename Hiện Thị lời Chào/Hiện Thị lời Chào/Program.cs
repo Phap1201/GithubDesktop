98984280c29e0024bd250751c5c756ff49f40df4
Phap1201 <hohongphap9a2@gmail.com>
2023-05-30 08:12:22 +0700

    bài tập hiện thì lời chào

---
+++ Hiện Thị lời Chào/Hiện Thị lời Chào/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiện_Thị_lời_Chào
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Moi Ban Nhap Ten:");
+            string Name=Console.ReadLine();
+            Console.WriteLine("Hello: " + Name);
+            Console.ReadLine();
+        }
+    }
+}
