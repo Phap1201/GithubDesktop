9456463dc219b11fb76fd2826df0814e21db026b
Phap1201 <hohongphap9a2@gmail.com>
2023-05-30 08:26:45 +0700

    UngDungChuyenDoiTienTe

---
+++ UngDungChuyenDoiTienTe/UngDungChuyenDoiTienTe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UngDungChuyenDoiTienTe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int USD = 1;
+            Console.WriteLine("MoiBanNhap USD:");
+            USD=int.Parse(Console.ReadLine());
+            int VND = 23 * USD;
+            Console.WriteLine(USD + "USD=" + VND + "K.VND");
+            Console.ReadLine();
+
+        }
+    }
+}
