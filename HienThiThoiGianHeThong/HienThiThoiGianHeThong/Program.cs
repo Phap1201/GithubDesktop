9b1eecc8a2f5acb1e32defbcdf4a9a3b658b3640
Phap1201 <hohongphap9a2@gmail.com>
2023-05-30 08:45:25 +0700

    HienThiThoiGianHeThon

---
+++ HienThiThoiGianHeThong/HienThiThoiGianHeThong/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HienThiThoiGianHeThong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime LocalDate= DateTime.Now;
+            Console.WriteLine("Datetime Now is:" + LocalDate);
+            Console.WriteLine("Datetime Now new is:" + string.Format("{0:dd/MM/yyyy HH:mm:ss}", LocalDate));
+            Console.ReadLine();
+        }
+    }
+}
