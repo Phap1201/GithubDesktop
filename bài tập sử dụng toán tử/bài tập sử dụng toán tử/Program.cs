09d102e1cb7a0799f3540b41e2577b4aa91546dd
Phap1201 <hohongphap9a2@gmail.com>
2023-05-30 08:00:13 +0700

    bài tập sử dụng toán tử

---
+++ bài tập sử dụng toán tử/bài tập sử dụng toán tử/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bài_tập_sử_dụng_toán_tử
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+            Console.Write("Moi ban nhap width:");
+            width=float.Parse(Console.ReadLine());
+            Console.Write("Moi ban nhap height:");
+            height=float.Parse(Console.ReadLine());
+            float area=width*height;
+            Console.WriteLine("Area is:" + area);
+            Console.ReadLine();
+        }
+    }
+}
